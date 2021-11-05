5d856423d9387ea5952e8fecdbcdac4447c075df
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-04 17:40:07 -0700

    js create getElem and started event listener

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
-
+const peacockAudio = document.getElementById('peacock-audio');
+const peacockImg = document.getElementById('peacock-img');
+const llamaAudio = document.getElementById('llama-audio');
+const llamaImg = document.getElementById('llama-img');
+const donkeyAudio = document.getElementById('donkey-audio');
+const donkeyImg = document.getElementById('donkey-img');
+const clickedOnElem = document.getElementById('clicked-on');
 // initialize global state
 
 // set event listeners 
